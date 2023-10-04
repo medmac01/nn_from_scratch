37419cf6186478328ec10b0644d4d4dc623d7352
medmac01 <[th3justhacker@gmail.com]>
2023-10-04 11:22:59 +0100

    Added repr fct + minor improvments

--- fc_layer.py
+++ fc_layer.py
@@ -1,14 +1,20 @@
 import numpy as np
 
 class fc_layer:
-    def __init__(self,inputs,in_size,out_size,name="Dense",activation="relu"):
+    def __init__(self,in_size,out_size,name="Dense",activation="relu"):
         self.weights_matrix = np.zeros(shape=(in_size,out_size), dtype=np.float16)
         self.layer_name = name
         self.activation = activation
         self.shape = (in_size,out_size)
-        self.inputs = inputs
 
-        assert inputs.shape[0] == int(in_size), "Inputs and out_size do not match"
+
+    def __repr__(self):
+        info = f'''
+        Layer name : {self.layer_name}
+        Size : {self.shape[1]}
+        Activation : {self.activation}
+        '''
+        return info
 
     def init_params(self):
         self.weights_matrix = np.random.uniform(size=self.shape)
@@ -21,8 +27,9 @@ class fc_layer:
         x[x < 0] = 0
         return x
 
-    def feed_forward(self):
+    def feed_forward(self,inputs):
         self.init_params()
-        weighted_sum = np.matmul(self.weights_matrix.T,self.inputs.T)
+        assert inputs.shape[0] == int(self.shape[0]), "Inputs and out_size do not match"
+        weighted_sum = np.matmul(self.weights_matrix.T,inputs.T)
         activations = self.sigmoid(weighted_sum) if self.activation == 'sigmoid' else self.relu(weighted_sum)
         return activations
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import fc_layer as fc
 import numpy as np
 
 
-dense_1 = fc.fc_layer(np.array([1,2,3]),3,5,"Dense_1","relu")
+dense_1 = fc.fc_layer(3,5,"Dense_1","relu")
+print(dense_1)
 print(dense_1.weights_matrix)
-activations = dense_1.feed_forward()
+activations = dense_1.feed_forward(np.array([1,2,4.1]))
 
 print(dense_1.weights_matrix)
 print(activations)
