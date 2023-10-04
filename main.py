4ad4d61981738f99decb0e2670c242284898b2d6
medmac01 <th3justhacker@gmail.com>
2023-10-04 12:32:09 +0100

    improvments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import nn
 
 dense_1 = fc.fc_layer(3,5,"Dense_1","relu")
-print(dense_1)
+#print(dense_1)
 #print(dense_1.weights_matrix)
 activations = dense_1.feed_forward(np.array([1,2,4.1]))
 
@@ -12,11 +12,14 @@ activations = dense_1.feed_forward(np.array([1,2,4.1]))
 
 
 model = nn.NN()
+
+model.add(fc.fc_layer(in_size=784, out_size=784, name="dense1", activation="relu"))
 model.add(fc.fc_layer(in_size=784, out_size=512, name="dense1", activation="relu"))
 model.add(fc.fc_layer(in_size=512, out_size=256, name="dense2", activation="relu"))
 model.add(fc.fc_layer(in_size=256, out_size=10, name="dense3", activation="relu"))
 
 inputs = np.random.random(size=784)
+print(model)
 #inputs = np.array([1, 2, 3, 4, 5], dtype=np.float16)
 
 # Forward pass through the network
--- nn.py
+++ nn.py
@@ -1,14 +1,24 @@
 import fc_layer as fc
+import tqdm
 class NN:
     def __init__(self,name="Model"):
         self.name = name
         self.layers= []
+
+    def __repr__(self):
+        return f'''
+---------------------
+Model name : {self.name}
+Model layers : {self.layers}
+---------------------
+    '''
     def add(self,layer):
         self.layers.append(layer)
 
+
     def forward(self, inputs):
         output = inputs
-        for layer in self.layers:
+        for layer in tqdm.tqdm(self.layers):
             output = layer.feed_forward(output) 
 
         return output
